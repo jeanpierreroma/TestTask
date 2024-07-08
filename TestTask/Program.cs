143a275cbd8e1f559df3a561c43cd40b1a8df451
Roman Chornyi <roman.chornyi.pz.2020@lpnu.ua>
2024-07-08 18:31:51 +0300

    Add project files.

---
+++ TestTask/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string f = "3+5*(10-5)+50-(5*20-10)";
+
+            StringHelper stringHelper = new StringHelper(f);
+
+            var r = stringHelper.ResolveExample();
+
+            Console.WriteLine(r);
+        }        
+    }
+}
